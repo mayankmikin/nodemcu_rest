94939d30802c794e085f190988359463b2f81d8a
LAPTOP-CVF082V0\UpstartCommerce <mayank.verma@upstartcommerce.com>
2018-12-02 14:32:57 +0530

    first commit .... basic spring rest apio layout

---
+++ src/main/java/com/dominion/nodemcu/NodemcuApplication.java
@@ -0,0 +1,12 @@
+package com.dominion.nodemcu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodemcuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NodemcuApplication.class, args);
+	}
+}
