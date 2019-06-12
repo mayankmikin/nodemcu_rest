f004af17da99cc0b4a8e416dde59b315dd36df38
Unknown <mayank.mikin@gmail.com>
2019-06-12 13:42:19 +0530

    adding an empty spring boot project

---
+++ src/main/java/com/whitehall/esp/microservices/SmartyApartmentsApplication.java
@@ -0,0 +1,15 @@
+package com.whitehall.esp.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+@SpringBootApplication
+//@EnableDiscoveryClient
+public class SmartyApartmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartyApartmentsApplication.class, args);
+	}
+	
+
+}
