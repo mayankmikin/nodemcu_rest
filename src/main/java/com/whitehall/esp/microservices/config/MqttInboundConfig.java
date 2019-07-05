95ab54ad7d18bd32671746816fc8dbc0dc8d2ccf
Himanshu Kotnala <himanshu.kotnala99@gmail.com>
2019-07-05 14:15:54 +0530

    Mqtt-sub working

---
+++ src/main/java/com/whitehall/esp/microservices/config/MqttInboundConfig.java
@@ -0,0 +1,85 @@
+package com.whitehall.esp.microservices.config;
+
+import org.eclipse.paho.client.mqttv3.IMqttAsyncClient;
+import org.eclipse.paho.client.mqttv3.IMqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessageProducer;
+import org.springframework.integration.mqtt.core.ConsumerStopAction;
+import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
+import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
+import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
+import org.springframework.integration.mqtt.support.MqttHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.MessagingException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Configuration
+public class MqttInboundConfig {
+
+	@Value("${spring.data.mqtt.uri}")
+	private String host;
+	
+	@Value("${spring.data.mqtt.topic}")
+	private String topic;
+	
+	@Value("${spring.data.mqtt.username}")
+	private String username;
+	
+	@Value("${spring.data.mqtt.password}")
+	private String password;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
+	
+    @Bean
+    public MessageChannel mqttInputChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    public MessageProducer inbound() {
+    	DefaultMqttPahoClientFactory clientFactory = new DefaultMqttPahoClientFactory();
+    	clientFactory.getConnectionOptions().setUserName(username);
+    	clientFactory.getConnectionOptions().setPassword(password.toCharArray());
+    	
+        MqttPahoMessageDrivenChannelAdapter adapter =
+                new MqttPahoMessageDrivenChannelAdapter(host, "nodeMCUService",clientFactory,"device/+");
+      
+      
+        adapter.setCompletionTimeout(5000);
+        adapter.setConverter(new DefaultPahoMessageConverter());
+        adapter.setQos(1);
+        adapter.setOutputChannel(mqttInputChannel());
+        return adapter;
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "mqttInputChannel")
+    public MessageHandler handler() {
+        return new MessageHandler() {
+
+            @Override
+            public void handleMessage(Message<?> message) throws MessagingException {
+            	//MqttHeaders header =  objectMapper.convertValue(message.getHeaders(),MqttHeaders.class);
+            	String topic = (String) message.getHeaders().get(MqttHeaders.RECEIVED_TOPIC);
+            	System.out.println("Topic:"+topic+"  Message:"+message.getPayload());
+            }
+
+        };
+    }
+
+}
--- src/main/java/com/whitehall/esp/microservices/controller/DeviceController.java
+++ src/main/java/com/whitehall/esp/microservices/controller/DeviceController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.whitehall.esp.microservices.config.MqttOutboundConfig;
 import com.whitehall.esp.microservices.exceptions.EntityNotFoundException;
 import com.whitehall.esp.microservices.model.Device;
 import com.whitehall.esp.microservices.services.DeviceService;
@@ -23,7 +24,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/device")
 @Slf4j
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class DeviceController {
@@ -37,64 +38,68 @@ public class DeviceController {
 	private DeviceService deviceService;
 	
 	/*
-	 * @GetMapping("/device/code/{Device}") public Mono<Device>
+	 * @GetMapping("/code/{Device}") public Mono<Device>
 	 * findByDevicecode(@PathVariable("Device") String code) {
 	 * log.info("findByDevice: DeviceCode={}", code); return
 	 * deviceService.findByDeviceCode(code); }
 	 */
 
 	
-	@GetMapping("/device")
+	@GetMapping("")
 	public Flux<Device> findAll() {
 		log.info("findAll");
 		return deviceService.findAll();
 	}
-	@GetMapping("/device/account/{accountId}")
+	@GetMapping("/account/{accountId}")
 	public Flux<Device> findAllWithAccounts(@PathVariable String accountId) {
 		log.info("findAllWithAccount id : {}",accountId);
 		return deviceService.findAllWithAccounts(accountId);
 	}
 
-	@GetMapping("/device/{id}")
+	@GetMapping("/{id}")
 	public Mono<Device> findById(@PathVariable("id") String id) throws EntityNotFoundException {
 		log.info("findById: id={}", id);
 		return deviceService.getDevice(id);
 	}
 
-	@PostMapping("/device")
+	@PostMapping("")
 	public Mono<Device> create(@Valid @RequestBody Device Device) throws JsonProcessingException, EntityNotFoundException {
 		log.info("create device : {}",mapper.writerWithDefaultPrettyPrinter().writeValueAsString(Device));
 		return deviceService.createDevice(Device);
 	}
 	
-	@GetMapping("/device/serialId/{serialId}")
+	@GetMapping("/serialId/{serialId}")
 	public Mono <Device> findBySerialId(@PathVariable String serialId) {
 		return deviceService.findBySerialId(serialId);
 	}
-	@GetMapping("/device/exporterId/{exporterId}")
+	@GetMapping("/exporterId/{exporterId}")
 	public Flux<Device> findByExporterId(@PathVariable String exporterId)
 	 {
 		 return deviceService.findByExporterId(exporterId);
 	 }
-	@GetMapping("/device/manufacturerId/{manufacturerId}")
+	@GetMapping("/manufacturerId/{manufacturerId}")
 	public  Flux<Device> findByManufacturerId(@PathVariable String manufacturerId)
 	{
 		log.info("manufacturerId : {}",manufacturerId);
 		return deviceService.findByManufacturerId(manufacturerId);
 	}
 	
-	@PatchMapping("/device/{serialId}/account/{accountId}")
+	@PatchMapping("/{serialId}/account/{accountId}")
 	public Mono<Device> changeAccountOfaDevice(@PathVariable String serialId,@PathVariable String accountId)throws EntityNotFoundException {
 		log.info("changeAccountOfaDevice :deviceId= {},accountId: {} ",serialId,accountId);
 		
 		return deviceService.changeAccountOfaDevice(serialId, accountId);
 	}
 	
-	@PatchMapping("/device")
+	@PatchMapping("")
 	public Mono<Device> editDevice(@Valid @RequestBody Device Device)throws EntityNotFoundException {
 		log.info("editDevice :deviceId= {},accountId: {} ",Device.getDeviceId(),Device.getAccount().getAccountId());
 		
 		return deviceService.editDevice(Device);
 	}
 	
+	@GetMapping("/test")
+	public void testMqtt() {
+		MqttOutboundConfig.sendToMqtt("hello world", "/device/abc");
+	}
 }
