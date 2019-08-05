42c2442a19cb8c3a8bf85dc70ddac69a0d1273ca
Mayank Verma <mayank.mikin@gmail.com>
2019-08-05 16:37:53 +0530

    added move decie into room

--- src/main/java/com/whitehall/esp/microservices/controller/BuildingInfoController.java
+++ src/main/java/com/whitehall/esp/microservices/controller/BuildingInfoController.java
@@ -1,18 +1,23 @@
 package com.whitehall.esp.microservices.controller;
 
+import java.util.stream.Collectors;
+
 import javax.security.auth.login.AccountNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.whitehall.esp.microservices.exceptions.BuildingInfoNotFoundException;
 import com.whitehall.esp.microservices.model.Account;
 import com.whitehall.esp.microservices.model.BuildingInfo;
+import com.whitehall.esp.microservices.model.frontend.MoveDeviceIntoRoom;
 import com.whitehall.esp.microservices.services.AccountService;
 import com.whitehall.esp.microservices.services.BuildingInfoService;
 
@@ -73,5 +78,29 @@ public class BuildingInfoController
 		log.info("getBuildingInfoByAccountAccountId {}",accountId);
 		return buildingInfoService.findByAccountAccountId(accountId);
 	}
+	
+	@PatchMapping("/moveDevice/accountId/{accountId}")
+	public Mono<BuildingInfo> moveDeviceIntoRoom(@RequestBody MoveDeviceIntoRoom devices,@PathVariable String accountId) 
+	{
+		log.info("moveDeviceIntoRoom accountId{}",accountId);
+		BuildingInfo buildingInfo=buildingInfoService.findById(devices.getBuildingInfoId()).block();
+		if(null== buildingInfo)
+		{
+			throw new BuildingInfoNotFoundException("invalid buildingInfo Id");
+		}
+		else
+		{
+			buildingInfo.getHouses().stream()
+			.flatMap(h->h.getFloor().stream()
+					).anyMatch(f-> f.getRooms().stream()
+							.filter(r-> r.getRoomId().equalsIgnoreCase(devices.getRoomId()))
+							.anyMatch(p-> p.getDevices().addAll(devices.getDeviceList()))
+							);
+			
+		}
+	
+		return buildingInfoService.createBuildingInfo(buildingInfo);
+	}
+	
 
 }
